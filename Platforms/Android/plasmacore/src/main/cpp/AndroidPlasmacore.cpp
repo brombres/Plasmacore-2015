66c127c2bc0f6d574b74ea3aa4c1debab443ea56
Abe Pralle <Abe.Pralle@gmail.com>
2018-05-28 12:10:01 -0700

    [Android] Additional Android Studio build infrastructure
    - Android Studio now invokes 'rogo android' as first step.
    - Placeholder native hooks.
    - RogueProgram.cpp compiles with no errors.
    - Need to add Java infrastructure.

--- Platforms/Android/plasmacore/src/main/cpp/AndroidPlasmacore.cpp
+++ Platforms/Android/plasmacore/src/main/cpp/AndroidPlasmacore.cpp
@@ -7,8 +7,12 @@ using namespace std;
 #define ROGUE_FN(return_type,class_name,fn_name) \
   extern "C" JNIEXPORT return_type JNICALL Java_org_plasmacore_##class_name##_##fn_name
 
+static JNIEnv* Plasmacore_jni_env;  // updated every Java JNI call
+
 ROGUE_FN( void, Rogue,nativeLaunch )( JNIEnv* env )
 {
+  ROGUE_LOG( "Launching Rogue..." );
+  Plasmacore_jni_env = env;
   try
   {
     Rogue_quit();  // reset global state if necessary
@@ -17,21 +21,42 @@ ROGUE_FN( void, Rogue,nativeLaunch )( JNIEnv* env )
   }
   catch (RogueException* err)
   {
-    printf( "Uncaught exception\n" );
+    ROGUE_LOG_ERROR( "Uncaught exception\n" );
     RogueException__display( err );
   }
 }
 
 ROGUE_FN( void, Rogue,nativeQuit )( JNIEnv* env )
 {
+  Plasmacore_jni_env = env;
   try
   {
     Rogue_quit();
   }
   catch (RogueException* err)
   {
-    printf( "Uncaught exception\n" );
+    ROGUE_LOG_ERROR( "Uncaught exception\n" );
     RogueException__display( err );
   }
 }
 
+extern "C" RogueString* Plasmacore_find_asset( RogueString* name )
+{
+  return NULL;
+}
+
+extern "C" RogueString* Plasmacore_get_user_data_folder()
+{
+  return RogueString_create_from_utf8( "." );
+}
+
+extern "C" RogueString* Plasmacore_get_application_data_folder()
+{
+  return RogueString_create_from_utf8( "." );
+}
+
+bool PlasmacoreMessage_send( RogueByte_List* data )
+{
+  ROGUE_LOG( "TODO: PlasmacoreMessage_send()" );
+  return false;
+}
