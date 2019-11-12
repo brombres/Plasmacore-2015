1de436eed07b419b33216298da6f4919093ef187
Abe Pralle <abe.pralle@gmail.com>
2019-11-11 21:28:33 -0800

    [Web] Initial blank screen is now black instead of yellow

--- Libraries/Plasmacore/SDL/PlasmacoreView.cpp
+++ Libraries/Plasmacore/SDL/PlasmacoreView.cpp
@@ -79,7 +79,7 @@ void PlasmacoreView::init ()
     return;
   }
 
-  glClearColor(1,1,0,1);
+  glClearColor(0,0,0,1);
   glClear(GL_COLOR_BUFFER_BIT);
 
   configure(); // Is there a reason not to call this here?
